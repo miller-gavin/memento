8f6e32a81ed03d73e1b651d1b9f6cb7fca4c74cd
Gavin M <gavin.miller@sinnerschrader.com>
2023-04-25 09:32:19 +0200

    build: add prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 //import dotenv from 'dotenv';
 
 //dotenv.config()
@@ -7,4 +7,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});

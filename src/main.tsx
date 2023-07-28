a548c483a7aa46422c9ac4519db1594384ba31c3
Gavin M <gavin.miller@sinnerschrader.com>
2023-07-28 09:59:28 +0200

    feat: wip amplify set up

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app";
+import { Amplify, Auth } from 'aws-amplify';
+//import awsconfig from './aws-exports';
+//Amplify.configure(awsconfig);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
